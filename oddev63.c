12ac28ad3568de597aa9d664fb6282124f7bfaa7
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-03-07 15:55:46 +0530

    Create oddev63.c

---
+++ oddev63.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter the number");
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("even");
+    }
+    else
+    printf("odd")
+    }
